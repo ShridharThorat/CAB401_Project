55c29900ec1e2601c14d21f5b85e2d12864123fa
ShridharThorat <shridhar.thorat01+github@gmail.com>
2023-09-08 19:29:39 +1000

    Added file for parallelisation and made minor changes. Added preliminary profiling files

---
+++ cvtree_project/cvtree_project/cvtree_parallel.cpp
@@ -0,0 +1,8 @@
+//
+//  cvtree_parallel.cpp
+//  cvtree_project
+//
+//  Created by Shridhar Thorat on 8/9/2023.
+//
+
+#include "cvtree_parallel.hpp"
---
+++ cvtree_project/cvtree_project/cvtree_parallel.hpp
@@ -0,0 +1,13 @@
+//
+//  cvtree_parallel.hpp
+//  cvtree_project
+//
+//  Created by Shridhar Thorat on 8/9/2023.
+//
+
+#ifndef cvtree_parallel_hpp
+#define cvtree_parallel_hpp
+
+#include <stdio.h>
+
+#endif /* cvtree_parallel_hpp */
