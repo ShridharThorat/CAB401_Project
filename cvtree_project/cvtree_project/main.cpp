092dbe8e423e1bba47afefa266d2275628adbf50
Shridhar <89955832+ShridharThorat@users.noreply.github.com>
2023-09-08 11:37:20 +1000

    Added Xcode project for cvtree project

---
+++ cvtree_project/cvtree_project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cvtree_project
+//
+//  Created by Shridhar Thorat on 8/9/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
