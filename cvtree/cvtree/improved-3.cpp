b71b1d075af9003756700f3ea57fba737af12b55
Shridhar <shridhar.thorat01+github@gmail.com>
2023-10-02 17:41:19 +1000

    Tried changing Bacteria class
    Timing is basically the same
    
    //    Bacteria Timing
    //    Sequential
    //    Loading Bacteria: 20 seconds
    //    CompareBacteria: 14 seconds
    //    Printing Compare: 0 seconds
    //    time elapsed: 34 seconds
    //
    //    Parallel: 8 Threads
    //    Loading Bacteria: 9 seconds
    //    CompareBacteria: 4 seconds
    //    Printing Compare: 0 seconds
    //    time elapsed: 13 seconds
    
    //    Bacteria_2 Timing
    //    Sequential
    //    Loading Bacteria: 21 seconds
    //    CompareBacteria: 13 seconds
    //    Printing Compare: 0 seconds
    //    time elapsed: 34 seconds
    //
    //    Parallel: 8 Threads
    //    Loading Bacteria: 8 seconds
    //    CompareBacteria: 5 seconds
    //    Printing Compare: 0 seconds
    //    time elapsed: 13 seconds

--- cvtree/cvtree/improved-3.cpp
+++ cvtree/cvtree/improved-3.cpp
@@ -14,23 +14,244 @@ void Init()
     M  = M1 *AA_NUMBER;            // M  = AA_NUMBER ^ (LEN);
 }
 
+void InitVectors(long* vector, long* second, long one_l[], long indexs, long total, long total_l, long complement)
+{
+    vector = new long [M];
+    second = new long [M1];
+    //        one_l = new long [AA_NUMBER];
+    memset(vector, 0, M * sizeof(long));
+    memset(second, 0, M1 * sizeof(long));
+    memset(one_l, 0, AA_NUMBER * sizeof(long));
+    total = 0;
+    total_l = 0;
+    complement = 0;
+}
+void init_buffer(char* buffer, long* second, long& complement, long& indexs, long one_l[], long& total_l)
+{
+    complement++;
+    indexs = 0;
+    for (int i=0; i<LEN-1; i++)
+    {
+        short enc = encode(buffer[i]);
+        one_l[enc]++;
+        total_l++;
+        indexs = indexs * AA_NUMBER + enc;
+    }
+    second[indexs]++;
+}
+void cont_buffer(char ch, long* vector, long* second, long one_l[], long& indexs, long& total, long& total_l, long& complement)
+{
+    short enc = encode(ch);
+    one_l[enc]++;
+    total_l++;
+    long index = indexs * AA_NUMBER + enc;
+    vector[index]++;
+    total++;
+    indexs = (indexs % M2) * AA_NUMBER + enc;
+    second[indexs]++;
+}
+
+class Bacteria_2
+{
+public:
+    long count;
+    double* tv;
+    long *ti;
+    
+    Bacteria_2(char* filename)
+    {
+        // Variables
+        long* vector;
+        long* second;
+        long one_l[AA_NUMBER];
+        //    long* one_l;
+        long indexs = 0;
+        long total;
+        long total_l;
+        long complement;
+        
+        // InitVectors
+        vector = new long [M];
+        second = new long [M1];
+        //        one_l = new long [AA_NUMBER];
+        memset(vector, 0, M * sizeof(long));
+        memset(second, 0, M1 * sizeof(long));
+        memset(one_l, 0, AA_NUMBER * sizeof(long));
+        total = 0;
+        total_l = 0;
+        complement = 0;
+//        InitVectors(vector, second, one_l, indexs, total, total_l, complement);
+        
+        // Get the file from directory
+        string directory = "/Users/Shridhar/Library/Developer/Xcode/XCodeProjects/CAB401_Project/cvtree/cvtree/data/";
+        string filePath = directory.append(filename);
+        
+        FILE* bacteria_file = fopen(filePath.c_str(), "r");
+        if(bacteria_file == NULL)
+        {
+            fprintf(stderr, "Error: failed to open file %s\n", filename);
+            exit(1);
+        }
+        // Read the file
+        char ch;
+        while ((ch = fgetc(bacteria_file)) != EOF)
+        {
+            if (ch == '>')
+            {
+                while (fgetc(bacteria_file) != '\n'); // skip rest of line
+                
+                char buffer[LEN-1];
+                fread(buffer, sizeof(char), LEN-1, bacteria_file);
+//                init_buffer(buffer);
+                init_buffer(buffer, second, complement, indexs, one_l, total_l);
+            }
+            else if (ch != '\n' && ch != '\t' && ch != '\r')
+                cont_buffer(ch, vector, second, one_l, indexs, total, total_l, complement);
+        }
+        
+        // Calculations
+        // not entirely sure but looks like `stochastic`
+        long total_plus_complement = total + complement;
+        double total_div_2 = total * 0.5;
+        int i_mod_aa_number = 0;
+        int i_div_aa_number = 0;
+        long i_mod_M1 = 0;
+        long i_div_M1 = 0;
+        
+        double one_l_div_total[AA_NUMBER];
+        for (int i=0; i<AA_NUMBER; i++)
+            one_l_div_total[i] = (double)one_l[i] / total_l;
+        
+        double* second_div_total = new double[M1];
+        for (int i=0; i<M1; i++)
+            second_div_total[i] = (double)second[i] / total_plus_complement;
+        
+        count = 0;
+        double* t = new double[M];
+        
+        for(long i=0; i<M; i++)
+        {
+            double p1 = second_div_total[i_div_aa_number];
+            double p2 = one_l_div_total[i_mod_aa_number];
+            double p3 = second_div_total[i_mod_M1];
+            double p4 = one_l_div_total[i_div_M1];
+            double stochastic =  (p1 * p2 + p3 * p4) * total_div_2;
+            
+            if (i_mod_aa_number == AA_NUMBER-1)
+            {
+                i_mod_aa_number = 0;
+                i_div_aa_number++;
+            }
+            else
+                i_mod_aa_number++;
+            
+            if (i_mod_M1 == M1-1)
+            {
+                i_mod_M1 = 0;
+                i_div_M1++;
+            }
+            else
+                i_mod_M1++;
+            
+            if (stochastic > EPSILON)
+            {
+                t[i] = (vector[i] - stochastic) / stochastic;
+                count++;
+            }
+            else
+                t[i] = 0;
+        }
+        
+        // Reset values
+        delete[] second_div_total;
+        delete vector;
+        delete second;
+        //        delete one_l;
+        
+        
+        tv = new double[count];
+        ti = new long[count];
+        
+        int pos = 0;
+        for (long i=0; i<M; i++)
+        {
+            if (t[i] != 0)
+            {
+                tv[pos] = t[i];
+                ti[pos] = i;
+                pos++;
+            }
+        }
+        delete[] t;
+        
+        fclose (bacteria_file);
+    }
+
+};
+
+double CompareBacteria_2(Bacteria_2* b1, Bacteria_2* b2)
+{
+    double correlation = 0;
+    double vector_len1=0;
+    double vector_len2=0;
+    long p1 = 0;
+    long p2 = 0;
+    while (p1 < b1->count && p2 < b2->count)
+    {
+        long n1 = b1->ti[p1];
+        long n2 = b2->ti[p2];
+        if (n1 < n2)
+        {
+            double t1 = b1->tv[p1];
+            vector_len1 += (t1 * t1);
+            p1++;
+        }
+        else if (n2 < n1)
+        {
+            double t2 = b2->tv[p2];
+            p2++;
+            vector_len2 += (t2 * t2);
+        }
+        else
+        {
+            double t1 = b1->tv[p1++];
+            double t2 = b2->tv[p2++];
+            vector_len1 += (t1 * t1);
+            vector_len2 += (t2 * t2);
+            correlation += t1 * t2;
+        }
+    }
+    while (p1 < b1->count)
+    {
+        double t1 = b1->tv[p1++];
+        vector_len1 += (t1 * t1);
+    }
+    while (p2 < b2->count)
+    {
+        double t2 = b2->tv[p2++];
+        vector_len2 += (t2 * t2);
+    }
+
+    return correlation / (sqrt(vector_len1) * sqrt(vector_len2));
+}
+
 class Bacteria
 {
 private:
     long* vector;
     long* second;
     long one_l[AA_NUMBER];
-//    long* one_l;
+    //    long* one_l;
     long indexs;
     long total;
     long total_l;
     long complement;
-
+    
     void InitVectors()
     {
         vector = new long [M];
         second = new long [M1];
-//        one_l = new long [AA_NUMBER];
+        //        one_l = new long [AA_NUMBER];
         memset(vector, 0, M * sizeof(long));
         memset(second, 0, M1 * sizeof(long));
         memset(one_l, 0, AA_NUMBER * sizeof(long));
@@ -38,7 +259,7 @@ private:
         total_l = 0;
         complement = 0;
     }
-
+    
     void init_buffer(char* buffer)
     {
         complement++;
@@ -52,7 +273,7 @@ private:
         }
         second[indexs]++;
     }
-
+    
     void cont_buffer(char ch)
     {
         short enc = encode(ch);
@@ -64,17 +285,17 @@ private:
         indexs = (indexs % M2) * AA_NUMBER + enc;
         second[indexs]++;
     }
-
+    
 public:
     long count;
     double* tv;
     long *ti;
-
-    Bacteria(char* filename)
+    
+    void Calculate(char* filename)
     {
         string directory = "/Users/Shridhar/Library/Developer/Xcode/XCodeProjects/CAB401_Project/cvtree/cvtree/data/";
         string filePath = directory.append(filename);
-
+        
         FILE* bacteria_file = fopen(filePath.c_str(), "r");
         if(bacteria_file == NULL)
         {
@@ -82,14 +303,14 @@ public:
             exit(1);
         }
         InitVectors();
-
+        
         char ch;
         while ((ch = fgetc(bacteria_file)) != EOF)
         {
             if (ch == '>')
             {
                 while (fgetc(bacteria_file) != '\n'); // skip rest of line
-
+                
                 char buffer[LEN-1];
                 fread(buffer, sizeof(char), LEN-1, bacteria_file);
                 init_buffer(buffer);
@@ -97,7 +318,7 @@ public:
             else if (ch != '\n' && ch != '\t' && ch != '\r')
                 cont_buffer(ch);
         }
-
+        
         // not entirely sure but looks like `stochastic`
         long total_plus_complement = total + complement;
         double total_div_2 = total * 0.5;
@@ -105,18 +326,18 @@ public:
         int i_div_aa_number = 0;
         long i_mod_M1 = 0;
         long i_div_M1 = 0;
-
+        
         double one_l_div_total[AA_NUMBER];
         for (int i=0; i<AA_NUMBER; i++)
             one_l_div_total[i] = (double)one_l[i] / total_l;
-
+        
         double* second_div_total = new double[M1];
         for (int i=0; i<M1; i++)
             second_div_total[i] = (double)second[i] / total_plus_complement;
-
+        
         count = 0;
         double* t = new double[M];
-
+        
         for(long i=0; i<M; i++)
         {
             double p1 = second_div_total[i_div_aa_number];
@@ -124,7 +345,7 @@ public:
             double p3 = second_div_total[i_mod_M1];
             double p4 = one_l_div_total[i_div_M1];
             double stochastic =  (p1 * p2 + p3 * p4) * total_div_2;
-
+            
             if (i_mod_aa_number == AA_NUMBER-1)
             {
                 i_mod_aa_number = 0;
@@ -132,7 +353,7 @@ public:
             }
             else
                 i_mod_aa_number++;
-
+            
             if (i_mod_M1 == M1-1)
             {
                 i_mod_M1 = 0;
@@ -140,7 +361,7 @@ public:
             }
             else
                 i_mod_M1++;
-
+            
             if (stochastic > EPSILON)
             {
                 t[i] = (vector[i] - stochastic) / stochastic;
@@ -149,17 +370,17 @@ public:
             else
                 t[i] = 0;
         }
-
+        
         // Reset values
         delete[] second_div_total;
         delete vector;
         delete second;
-//        delete one_l;
-
-
+        //        delete one_l;
+        
+        
         tv = new double[count];
         ti = new long[count];
-
+        
         int pos = 0;
         for (long i=0; i<M; i++)
         {
@@ -171,8 +392,13 @@ public:
             }
         }
         delete[] t;
-
+        
         fclose (bacteria_file);
+    }
+    
+    Bacteria(char* filename)
+    {
+        Calculate(filename);
     }
 };
 
@@ -245,6 +471,7 @@ double CompareBacteria(Bacteria* b1, Bacteria* b2)
     return correlation / (sqrt(vector_len1) * sqrt(vector_len2));
 }
 
+// Doesn't work
 double CompareBacteria_parallel(Bacteria* b1, Bacteria* b2)
 {
     double correlation = 0;
@@ -424,6 +651,7 @@ double CompareBacteria_parallel_2(int num_threads, Bacteria* b1, Bacteria* b2)
     return correlation / (sqrt(vector_len1) * sqrt(vector_len2));
 }
 
+// Doesn't work
 double CompareBacteria_parallel_3(int num_threads, Bacteria* b1, Bacteria* b2)
 {
     double correlation = 0;
@@ -567,14 +795,42 @@ void CompareAllBacteria()
     // num_threads = 16: 15 seconds
     
     // Para-02-version-2: Same speedup compared to version-1
-    Bacteria** b = new Bacteria*[number_bacteria];
+//    Bacteria** b = new Bacteria*[number_bacteria];
+    Bacteria_2** b_2 = new Bacteria_2*[number_bacteria];
+
+//    Bacteria Timing
+//    Sequential
+//    Loading Bacteria: 20 seconds
+//    CompareBacteria: 14 seconds
+//    Printing Compare: 0 seconds
+//    time elapsed: 34 seconds
+//
+//    Parallel: 8 Threads
+//    Loading Bacteria: 9 seconds
+//    CompareBacteria: 4 seconds
+//    Printing Compare: 0 seconds
+//    time elapsed: 13 seconds
+    
+//    Bacteria_2 Timing
+//    Sequential
+//    Loading Bacteria: 21 seconds
+//    CompareBacteria: 13 seconds
+//    Printing Compare: 0 seconds
+//    time elapsed: 34 seconds
+//
+//    Parallel: 8 Threads
+//    Loading Bacteria: 8 seconds
+//    CompareBacteria: 5 seconds
+//    Printing Compare: 0 seconds
+//    time elapsed: 13 seconds
     
     time_t t1 = time(NULL);
-    #pragma omp parallel for num_threads(num_threads)
+//    #pragma omp parallel for num_threads(num_threads)
     for (int i = 0; i<number_bacteria; i++)
     {
-        printf("load %d of %d RUNNING ON THREAD: %d \n", i + 1, number_bacteria, omp_get_thread_num());
-        b[i] = new Bacteria(bacteria_name[i]);
+        printf("load %d of %d RUNNING ON THREAD: %d \n", i + 1, number_bacteria, omp_get_thread_num()+1);
+//        b[i] = new Bacteria(bacteria_name[i]);
+        b_2[i] = new Bacteria_2(bacteria_name[i]);
     }
     time_t t2 = time(NULL);
     printf("Loading Bacteria: %ld seconds\n", t2 - t1);
@@ -582,14 +838,15 @@ void CompareAllBacteria()
     // Para-01: Straightforward parallelisation
     time_t t3 = time(NULL);
     vector<tuple<int, int, double>> correlations;
-    #pragma omp parallel for num_threads(num_threads)
+//    #pragma omp parallel for num_threads(num_threads)
     for(int i=0; i<number_bacteria-1; i++)
     {
         // 13 seconds with Para-01 here & Para-02
 //        #pragma omp parallel for num_threads(2)
         for(int j=i+1; j<number_bacteria; j++)
         {
-            double correlation = CompareBacteria(b[i], b[j]);
+//            double correlation = CompareBacteria(b[i], b[j]);
+            double correlation = CompareBacteria_2(b_2[i], b_2[j]);
 //            double correlation = CompareBacteria_parallel_2(2, b[i], b[j]);
             correlations.push_back(make_tuple(i, j, correlation));
         }
@@ -635,9 +892,8 @@ void Delay()
 
 int main(int argc,char * argv[])
 {
-    Delay();
+//    Delay();
     time_t t1 = time(NULL);
-
     Init();
     ReadInputFile("/Users/Shridhar/Library/Developer/Xcode/XCodeProjects/CAB401_Project/cvtree/cvtree/list.txt");
     CompareAllBacteria();
